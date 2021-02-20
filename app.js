dc5d0b71b0fff8e20f0175d8ee2fbb0085542e32
tuminzee <t.sheth33@gmail.com>
2021-02-20 13:26:58 +0530

    Add Cors

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var productRouter = require('./routes/product.route');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -22,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', productRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
