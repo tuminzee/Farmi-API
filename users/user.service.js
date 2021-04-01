ce4dd91074ca52c321dc156d1c7e54f2c1c89cfc
tuminzee <t.sheth33@gmail.com>
2021-04-01 21:21:24 +0530

    Add role based auth

--- users/user.service.js
+++ users/user.service.js
@@ -16,7 +16,7 @@ module.exports = {
 async function authenticate({ username, password }) {
     const user = await User.findOne({ username });
     if (user && bcrypt.compareSync(password, user.hash)) {
-        const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+        const token = jwt.sign({ sub: user.id, role: user.role }, config.secret, { expiresIn: '7d' });
         return {
             ...user.toJSON(),
             token
--- users/users.controller.js
+++ users/users.controller.js
@@ -1,11 +1,14 @@
 ﻿const express = require('express');
 const router = express.Router();
 const userService = require('./user.service');
+const authorize = require('_helpers/authorize');
+const Role = require('_helpers/role');
+
 
 // routes
 router.post('/authenticate', authenticate);
 router.post('/register', register);
-router.get('/', getAll);
+router.get('/', authorize(Role.Admin),  getAll);
 router.get('/current', getCurrent);
 router.get('/:id', getById);
 router.put('/:id', update);
@@ -31,17 +34,18 @@ function getAll(req, res, next) {
         .catch(err => next(err));
 }
 
+
 function getCurrent(req, res, next) {
     userService.getById(req.user.sub)
         .then(user => user ? res.json(user) : res.sendStatus(404))
         .catch(err => next(err));
 }
 
-function getById(req, res, next) {
-    userService.getById(req.params.id)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
-}
+// function getById(req, res, next) {
+//     userService.getById(req.params.id)
+//         .then(user => user ? res.json(user) : res.sendStatus(404))
+//         .catch(err => next(err));
+// }
 
 function update(req, res, next) {
     userService.update(req.params.id, req.body)
@@ -52,5 +56,20 @@ function update(req, res, next) {
 function _delete(req, res, next) {
     userService.delete(req.params.id)
         .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+
+function getById(req, res, next) {
+    const currentUser = req.user;
+    const id = parseInt(req.params.id);
+
+    // only allow admins to access other user records
+    if (id !== currentUser.sub && currentUser.role !== Role.Admin) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    userService.getById(req.params.id)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
         .catch(err => next(err));
 }
